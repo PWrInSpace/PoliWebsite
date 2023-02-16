3b3ff02af5daa6bdeb5e762cbfc2b7ea8cb12e2a
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-17 00:21:24 +0100

    update about us

--- src/pages/main-page/AboutUsSection.tsx
+++ src/pages/main-page/AboutUsSection.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import styles from './assets/about-us-section.module.scss';
+import FirstPlanet from '../../components/planets/FirstPlanet';
+import SecondPlanet from '../../components/planets/SecondPlanet';
+import ThirdPlanet from '../../components/planets/ThirdPlanet';
 
 export default function AboutUsSection(props: any) {
     return (
         <div className={styles.sectionContainer}>
-            O NAS
-
+            <div className={styles.sectionWrapper}>
+                <div className={styles.sectionTitle}>
+                    {__('aboutUs.header')}
+                </div>
+                <div className={styles.sectionDivider}></div>
+                <div className={styles.sectionContent}>
+                    {__('aboutUs.text')}
+                </div>
+                <div className={styles.sectionPlanets}>
+                    <FirstPlanet text={__('aboutUs.planet1')}/>
+                    <SecondPlanet text={__('aboutUs.planet2')}/>
+                    <ThirdPlanet text={__('aboutUs.planet3')}/>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/main-page/assets/about-us-section.module.scss.d.ts
+++ src/pages/main-page/assets/about-us-section.module.scss.d.ts
@@ -1,4 +1,11 @@
 declare const classNames: {
+  readonly bevietnampro: "bevietnampro";
+  readonly alata: "alata";
   readonly sectionContainer: "sectionContainer";
+  readonly sectionWrapper: "sectionWrapper";
+  readonly sectionTitle: "sectionTitle";
+  readonly sectionDivider: "sectionDivider";
+  readonly sectionContent: "sectionContent";
+  readonly sectionPlanets: "sectionPlanets";
 };
 export = classNames;
