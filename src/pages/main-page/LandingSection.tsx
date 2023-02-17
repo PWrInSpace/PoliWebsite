ff481d0338e8c6b470de01e5ae440f3c5c517da6
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-17 18:03:07 +0100

    create layout

--- src/pages/main-page/LandingSection.tsx
+++ src/pages/main-page/LandingSection.tsx
@@ -13,6 +13,45 @@ export default function LandingSection(props: any) {
                 <img src={mainBackgroundOverlay} className={styles.overlay}/>
                 <IconCircle/>
             </div>
+            <div className={styles.sectionWrapper}>
+                <div className={styles.sectionTitle}>
+                    {__('landingSection.title')}
+                </div>
+                <div className={styles.sectionDescription}>
+                    {__('landingSection.description')}
+                </div>
+                <div className={styles.sectionButtons}>
+                    <div className={styles.sectionButton}>
+                        {__('landingSection.buttons.projects')}
+                    </div>
+                    <div className={classes('styles.sectionButton', 'styles.sectionProjectButton')}>
+                        {__('landingSection.buttons.sponsors')}
+                    </div>
+                </div>
+                <div className={styles.sectionNumberContainer}>
+                    <div className={classes('styles.sectionMemberNumber', 'styles.sectionNumberTitle')}>
+                        <div className={styles.sectionNumber}>
+                            60
+                        </div>
+                        {__('landingSection.numbers.members')}
+                    </div>
+                    <div className={classes('styles.sectionYearNumber', 'styles.sectionNumberTitle')}>
+                        <div className={styles.sectionNumber}>
+                            2017
+                        </div>
+                        {__('landingSection.numbers.year')}
+                    </div>
+                    <div className={classes('styles.sectionStartNumber', 'styles.sectionNumberTitle')}>
+                        <div className={styles.sectionNumber}>
+                            12
+                        </div>
+                        {__('landingSection.numbers.starts')}
+                    </div>
+                </div>
+                <div className={styles.sectionArrow}>
+
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/main-page/assets/landing-section.module.scss.d.ts
+++ src/pages/main-page/assets/landing-section.module.scss.d.ts
@@ -2,5 +2,17 @@ declare const classNames: {
   readonly sectionContainer: "sectionContainer";
   readonly background: "background";
   readonly overlay: "overlay";
+  readonly sectionWrapper: "sectionWrapper";
+  readonly sectionTitle: "sectionTitle";
+  readonly sectionDescription: "sectionDescription";
+  readonly sectionButtons: "sectionButtons";
+  readonly sectionButton: "sectionButton";
+  readonly sectionNumberContainer: "sectionNumberContainer";
+  readonly sectionNumber: "sectionNumber";
+  readonly sectionNumberTitle: "sectionNumberTitle";
+  readonly sectionMemberNumber: "sectionMemberNumber";
+  readonly sectionYearNumber: "sectionYearNumber";
+  readonly sectionStartNumber: "sectionStartNumber";
+  readonly sectionArrow: "sectionArrow";
 };
 export = classNames;
