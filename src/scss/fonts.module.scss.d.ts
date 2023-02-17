e28f048aa4d7c756a8941dbcf563336969268532
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-17 18:02:58 +0100

    add fonts & text

--- src/scss/fonts.module.scss.d.ts
+++ src/scss/fonts.module.scss.d.ts
@@ -1,5 +1,7 @@
 declare const classNames: {
   readonly bevietnampro: "bevietnampro";
   readonly alata: "alata";
+  readonly kronaOne: "kronaOne";
+  readonly alegreyaSans: "alegreyaSans";
 };
 export = classNames;
