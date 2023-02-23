d8c8fcb05efd4ebaf177b30e4e6bf50c8efd44b9
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-23 23:14:51 +0100

    update main file

--- src/pages/main-page/OurProjects.tsx
+++ src/pages/main-page/OurProjects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './assets/our-projects.module.scss';
 import background from '../../assets/ourProjectsBackground.png';
+import RocketPanel from '../../components/rocket-panel/RocketPanel';
 
 const rockets = [
     'R1',
@@ -36,7 +37,22 @@ export default function OurProjects() {
 
     const chooseRocketPanel = () => {
         switch (selectedButton) {
-
+        case 'R1':
+            break;
+        case 'R2':
+            break;
+        case 'R3':
+            break;
+        case 'R2S':
+            break;
+        case 'R4S':
+            break;
+        case 'R4':
+            break;
+        case 'R4V2':
+            return <RocketPanel rocket={selectedButton} length={'4084 MM'} thrust={'2500 N'} weight={'35 KG'} velocity={'272 M/S'}/>;
+        case 'R5':
+            break;
         }
     };
 
@@ -51,6 +67,7 @@ export default function OurProjects() {
                 <div className={styles.sectionButtonContainer}>
                     {createButtons()}
                 </div>
+                {chooseRocketPanel()}
             </div>
         </div>
     );
