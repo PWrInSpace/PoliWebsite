29a9e93c3a900d307bd60e8652ff33cf0271a610
Igor Stalmach <igorstalmach@outlook.com>
2023-02-23 20:40:51 +0100

    Add about us section (#5)
    * add text to locales
    
    * add planets
    
    * update about us
    
    * fix about us mobile view
    
    * fix mobile view
    
    * move planets to icons & move divs to AboutUs.tsx
    
    * revert breakpoint
    
    * add PlanetComponent
    
    * review fix
    
    ---------
    
    Co-authored-by: JanZmyslowski <37934721+JanZmyslowski@users.noreply.github.com>

--- src/pages/main-page/AboutUsSection.tsx
+++ src/pages/main-page/AboutUsSection.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import styles from './assets/about-us-section.module.scss';
+import FirstPlanet from '../../icons/FirstPlanet';
+import SecondPlanet from '../../icons/SecondPlanet';
+import ThirdPlanet from '../../icons/ThirdPlanet';
+import PlanetComponent from '../../components/planet-component/PlanetComponent';
 
-export default function AboutUsSection(props: any) {
+export default function AboutUsSection() {
     return (
         <div className={styles.sectionContainer}>
-            O NAS
-
+            <div className={styles.sectionWrapper}>
+                <div className={styles.sectionTitle}>
+                    {__('aboutUs.header')}
+                </div>
+                <div className={styles.sectionDivider}></div>
+                <div className={styles.sectionContent}>
+                    {__('aboutUs.text')}
+                </div>
+                <div className={styles.sectionPlanets}>
+                    <PlanetComponent icon={<FirstPlanet/>} text={__('aboutUs.planet1')}/>
+                    <PlanetComponent icon={<SecondPlanet/>} text={__('aboutUs.planet2')}/>
+                    <PlanetComponent icon={<ThirdPlanet/>} text={__('aboutUs.planet3')}/>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/main-page/assets/about-us-section.module.scss.d.ts
+++ src/pages/main-page/assets/about-us-section.module.scss.d.ts
@@ -1,4 +1,11 @@
 declare const classNames: {
+  readonly bevietnampro: "bevietnampro";
+  readonly alata: "alata";
   readonly sectionContainer: "sectionContainer";
+  readonly sectionWrapper: "sectionWrapper";
+  readonly sectionPlanets: "sectionPlanets";
+  readonly sectionTitle: "sectionTitle";
+  readonly sectionDivider: "sectionDivider";
+  readonly sectionContent: "sectionContent";
 };
 export = classNames;
