0612ac3adf948473c228fcb6863d12a2cb0d3b3d
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-23 21:58:50 +0100

    add fonts

--- src/scss/fonts.module.scss.d.ts
+++ src/scss/fonts.module.scss.d.ts
@@ -1,5 +1,8 @@
 declare const classNames: {
   readonly bevietnampro: "bevietnampro";
   readonly alata: "alata";
+  readonly kronaone: "kronaone";
+  readonly alegreyasans: "alegreyasans";
+  readonly mulish: "mulish";
 };
 export = classNames;
