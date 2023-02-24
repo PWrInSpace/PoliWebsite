a287286b230683d6a4c8673d064c2e8b0348c77d
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-24 22:27:53 +0100

    create person picture component

---
+++ src/components/person-picture/assets/person-picture.module.scss.d.ts
@@ -0,0 +1,12 @@
+declare const classNames: {
+  readonly bevietnampro: "bevietnampro";
+  readonly alata: "alata";
+  readonly kronaone: "kronaone";
+  readonly alegreyasans: "alegreyasans";
+  readonly mainblue: "mainblue";
+  readonly container: "container";
+  readonly title: "title";
+  readonly name: "name";
+  readonly mail: "mail";
+};
+export = classNames;
