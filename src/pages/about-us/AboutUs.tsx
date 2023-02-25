e9989f9acc9adc6df1eea1605da617b9c26bb38e
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-02-25 16:54:32 +0100

    move paragraph info to an object

--- src/pages/about-us/AboutUs.tsx
+++ src/pages/about-us/AboutUs.tsx
@@ -7,7 +7,50 @@ import IconCircle from '../../icons/IconCircle';
 import AboutUsParagraph from '../../components/about-us-paragraph/AboutUsParagraph';
 import NumberContainerComponent from '../../components/number-container-component/NumberContainerComponent';
 
+const paragraphs = {
+    'firstParagraph': {
+        title: 'aboutUsPage.firstParagraph.title',
+        subtitle: 'aboutUsPage.firstParagraph.subtitle',
+        text: 'aboutUsPage.firstParagraph.text',
+        image: {
+            src: firstImage,
+            alt: 'PWr in Space crew',
+            position: 'right'
+        }
+    },
+    'secondParagraph': {
+        title: 'aboutUsPage.secondParagraph.title',
+        subtitle: 'aboutUsPage.secondParagraph.subtitle',
+        text: 'aboutUsPage.secondParagraph.text',
+        image: {
+            src: secondImage,
+            alt: 'PWr in Space crew',
+            position: 'left'
+        }
+    }
+};
+
 export default function AboutUs() {
+    const createParagraph = () => {
+        return Object.keys(paragraphs).map((paragraph) => {
+            if (paragraphs[paragraph].image.position === 'right') {
+                return (
+                    <div className={styles.paragraph}>
+                        <AboutUsParagraph title={__(paragraphs[paragraph].title)} subtitle={__(paragraphs[paragraph].subtitle)} text={__(paragraphs[paragraph].text)} wrapper={styles.firstWrapper}/>
+                        <img src={paragraphs[paragraph].image.src} alt={paragraphs[paragraph].image.alt} className={styles.paragraphImage}/>
+                    </div>
+                );
+            } else if (paragraphs[paragraph].image.position === 'left') {
+                return (
+                    <div className={styles.paragraph}>
+                        <img src={paragraphs[paragraph].image.src} alt={paragraphs[paragraph].image.alt} className={styles.paragraphImage}/>
+                        <AboutUsParagraph title={__(paragraphs[paragraph].title)} subtitle={__(paragraphs[paragraph].subtitle)} text={__(paragraphs[paragraph].text)} wrapper={styles.secondWrapper}/>
+                    </div>
+                );
+            }
+        });
+    };
+
     return (
         <div className={styles.container}>
             <div>
@@ -24,14 +67,7 @@ export default function AboutUs() {
                     {__('aboutUsPage.header')}
                 </div>
                 <div className={styles.divider}></div>
-                <div className={styles.paragraph}>
-                    <AboutUsParagraph title={__('aboutUsPage.firstParagraph.title')} subtitle={__('aboutUsPage.firstParagraph.subtitle')} text={__('aboutUsPage.firstParagraph.text')} wrapper={styles.firstWrapper}/>
-                    <img src={firstImage} alt="PWr in Space crew" className={styles.paragraphImage} />
-                </div>
-                <div className={styles.paragraph}>
-                    <img src={secondImage} alt="PWr in Space crew" className={styles.paragraphImage} />
-                    <AboutUsParagraph title={__('aboutUsPage.secondParagraph.title')} subtitle={__('aboutUsPage.secondParagraph.subtitle')} text={__('aboutUsPage.secondParagraph.text')} wrapper={styles.secondWrapper}/>
-                </div>
+                {createParagraph()}
                 <div className={styles.numbersContainer}>
                     <NumberContainerComponent number={1234} title={__('aboutUsPage.numbers.firstNumber')} />
                     <NumberContainerComponent number={1234} title={__('aboutUsPage.numbers.secondNumber')} />
