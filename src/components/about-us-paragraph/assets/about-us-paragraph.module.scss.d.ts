05cfc58bc52961721a20adf1fb9f6f28aa3dd425
JanZmyslowski <37934721+JanZmyslowski@users.noreply.github.com>
2023-02-26 23:43:33 +0100

    navbar improvement

--- src/components/about-us-paragraph/assets/about-us-paragraph.module.scss.d.ts
+++ src/components/about-us-paragraph/assets/about-us-paragraph.module.scss.d.ts
@@ -3,6 +3,7 @@ declare const classNames: {
   readonly alata: "alata";
   readonly kronaone: "kronaone";
   readonly alegreyasans: "alegreyasans";
+  readonly mulish: "mulish";
   readonly textWrapper: "textWrapper";
   readonly title: "title";
   readonly subtitle: "subtitle";
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NavMenuItem } from 'src/Contract';
 import IconLogo from '../../../src/icons/IconLogo';
 import NavBarItem from './NavBarItem';
@@ -6,29 +6,29 @@ import styles from './assets/navbar.module.scss';
 import IconMenu from '../../../src/icons/IconMenu';
 import IconClose from '../../../src/icons/IconClose';
 import SocialMediaComponentNoBackground from '../social-media-component/SocialMediaComponentNoBackground';
+import useWindowScroll from '../../hooks/useWindowScroll';
 
 interface ISelfProps {
     menuItems: NavMenuItem[];
 }
 
 export default function NavBar(props: ISelfProps) {
-
     const [menuOpen, setMenuOpen] = useState(false);
+    const scrollPosition = useWindowScroll();
 
     return (
-        <div className={styles.headerContainer}>
-            <div className={styles.headerWrapper}>
+        <div className={classes(styles.headerContainer, scrollPosition?.scrollTop > 10 && styles.withBlackBackground)}>
+            <div className={classes(styles.headerWrapper, menuOpen && styles.withBlackBackground)}>
                 <IconLogo onClick={() => window.location.href = window.appContext.baseUrl}/>
                 <div className={classes(styles.linksWrapper, !menuOpen && styles.linksHidden)}>
                     <div className={styles.menu} onClick={() => setMenuOpen(!menuOpen)}>
-                        {!menuOpen && <IconMenu/>}
-                        {menuOpen && <IconClose/>}
+                        {!menuOpen && <IconMenu color='white' size={42}/>}
+                        {menuOpen && <IconClose color='white' size={42}/>}
                     </div>
                     {props.menuItems.map(item => <NavBarItem item={item} />)}
                     {menuOpen && <SocialMediaComponentNoBackground/>}
                 </div>
             </div>
-        </div>
-        
+        </div>        
     );
 }
