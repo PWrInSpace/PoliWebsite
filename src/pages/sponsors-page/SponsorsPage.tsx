44853455127a1108ec01774c09e79a9b632df262
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-03-08 00:49:11 +0100

    remove white background & add links

--- src/pages/sponsors-page/SponsorsPage.tsx
+++ src/pages/sponsors-page/SponsorsPage.tsx
@@ -16,47 +16,58 @@ import manus from '../../assets/sponsors/manus.png';
 const images = {
     'airproducts': {
         src: airproducts,
-        alt: 'Air Products logo'
+        alt: 'Air Products logo',
+        webpage: 'https://www.airproducts.com.pl/'
     },
     'ataszek': {
         src: ataszek,
-        alt: 'Ataszek logo'
+        alt: 'Ataszek logo',
+        webpage: 'https://ataszek.pl/'
     },
     'computercontrols': {
         src: computercontrols,
-        alt: 'Computer Controls logo'
+        alt: 'Computer Controls logo',
+        webpage: 'https://www.ccontrols.net/'
     },
     'ilot': {
         src: ilot,
-        alt: 'ILot logo'
+        alt: 'ILot logo',
+        webpage: 'https://ilot.lukasiewicz.gov.pl/'
     },
     'mitutoyo': {
         src: mitutoyo,
-        alt: 'Mitutoyo logo'
+        alt: 'Mitutoyo logo',
+        webpage: 'https://mitutoyo.pl/'
     },
     'solidexpert': {
         src: solidexpert,
-        alt: 'Solid Expert logo'
+        alt: 'Solid Expert logo',
+        webpage: 'https://solidexpert.com/'
     },
     'wenagroup': {
         src: wenagroup,
-        alt: 'Wena Group logo'
+        alt: 'Wena Group logo',
+        webpage: 'http://www.wenagroup.com/'
     },
     'pwr': {
         src: pwr,
-        alt: 'Politechnika Wrocławska'
+        alt: 'Politechnika Wrocławska',
+        webpage: 'https://pwr.edu.pl/'
     },
     'w12n': {
         src: w12n,
-        alt: 'Wydział Elektroniki, Fotoniki i Mikrosystemów Politechniki Wrocławskiej'
+        alt: 'Wydział Elektroniki, Fotoniki i Mikrosystemów Politechniki Wrocławskiej',
+        webpage: 'https://wefim.pwr.edu.pl/'
     },
     'w10': {
         src: w10,
-        alt: 'Wydział Mechaniczny Politechniki Wrocławskiej'
+        alt: 'Wydział Mechaniczny Politechniki Wrocławskiej',
+        webpage: 'https://wm.pwr.edu.pl/'
     },
     'manus': {
         src: manus,
-        alt: 'Fundacja Manus przy Politechnice Wrocławskiej'
+        alt: 'Fundacja Manus przy Politechnice Wrocławskiej',
+        webpage: 'https://manus.pl/'
     }
 };
 
@@ -64,7 +75,7 @@ export default function SponsorsPage() {
     const createLogo = () => {
         return Object.keys(images).map((logo, key) => {
             return(
-                <img src={images[logo].src} alt={images[logo].alt} className={styles.logo} key={key}/>
+                <img src={images[logo].src} alt={images[logo].alt} className={styles.logo} key={key} onClick={() => window.open(images[logo].webpage, '_blank')}/>
             );
         });
     };
