1363da67415ffdf6e1d37e96f9ac6d8a7f121982
Igor Stalmach <igorstalmach@outlook.com>
2023-08-27 18:27:44 +0200

    add a Linkedin button

--- src/components/social-media-component/SocialMediaComponent.tsx
+++ src/components/social-media-component/SocialMediaComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './assets/social-media-component.module.scss';
 import FacebookIcon from '../../assets/icons/FacebookIcon';
 import InstagramIcon from '../../assets/icons/InstagramIcon';
+import LinkedInIcon from '../../assets/icons/LinkedInIcon';
 
 export default function SocialMediaComponent() {
     return(
@@ -9,6 +10,10 @@ export default function SocialMediaComponent() {
             <div className={styles.socialMediaWrapper}>
                 <FacebookIcon onClick={() => window.open('https://www.facebook.com/pwrinspace', '_blank')}/>
                 <InstagramIcon onClick={() => window.open('https://www.instagram.com/poliwrocket/', '_blank')}/>
+                <LinkedInIcon
+                    onClick={() => window.open('https://www.linkedin.com/company/pwr-in-space/', '_blank')}
+                    color={{ primaryColor: 'black', secondaryColor: 'white' }}
+                />
             </div>
         </div>
     );
--- src/components/social-media-component/SocialMediaComponentNoBackground.tsx
+++ src/components/social-media-component/SocialMediaComponentNoBackground.tsx
@@ -2,12 +2,17 @@ import styles from './assets/social-media-component-no-background.module.scss';
 import FacebookIcon from '../../assets/icons/FacebookIcon';
 import InstagramIcon from '../../assets/icons/InstagramIcon';
 import React from 'react';
+import LinkedInIcon from '../../assets/icons/LinkedInIcon';
 
 export default function SocialMediaComponentNoBackground() {
     return(
         <div className={styles.socialMediaContainerNoBackground}>
             <FacebookIcon onClick={() => window.open('https://www.facebook.com/pwrinspace', '_blank')}/>
             <InstagramIcon onClick={() => window.open('https://www.instagram.com/poliwrocket/', '_blank')}/>
+            <LinkedInIcon
+                onClick={() => window.open('https://www.linkedin.com/company/pwr-in-space/', '_blank')}
+                color={{ primaryColor: 'black', secondaryColor: 'white' }}
+            />
         </div>
     );
 }
