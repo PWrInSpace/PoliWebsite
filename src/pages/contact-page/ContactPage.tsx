9759e19e5d01f25f55e6621b385fddf5fd08d08d
Mikołaj <mikolaj.skjiba@gmail.com>
2026-07-28 07:45:36 +0200

    Update ContactPage.tsx

--- src/pages/contact-page/ContactPage.tsx
+++ src/pages/contact-page/ContactPage.tsx
@@ -89,6 +89,7 @@ export const ContactPage = () => {
               header={__(Locations.workshop.header)}
               text={Locations.workshop.text}
               map={Locations.workshop.map}
+              
             />
           </div>
         </>
